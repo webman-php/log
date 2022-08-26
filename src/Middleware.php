a3d26b90e2f495bbe7ee0a3a1b783b7ac6f04b76
walkor <walkor@users.noreply.github.com>
2022-08-26 22:09:13 +0800

    Update Middleware.php

--- src/Middleware.php
+++ src/Middleware.php
@@ -4,9 +4,12 @@ namespace Webman\Log;
 use support\Db;
 use support\Log;
 use support\Redis;
+use think\db\connector\Mysql;
+use think\facade\Db as ThinkDb;
 use Webman\Http\Request;
 use Webman\Http\Response;
 use Webman\MiddlewareInterface;
+use Illuminate\Database\Connection;
 use Illuminate\Database\Events\QueryExecuted;
 use Illuminate\Redis\Events\CommandExecuted;
 use Throwable;
@@ -25,14 +28,15 @@ class Middleware implements MiddlewareInterface
      */
     public function process(Request $request, callable $next) : Response
     {
-        static $initialized;
+        static $initialized_db, $initialized_redis;
         $start_time = microtime(true);
         $logs = $request->getRealIp() . ' ' . $request->method() . ' ' . trim($request->fullUrl(), '/');
         $this->logs = '';
-        if (class_exists(\think\facade\Db::class)) {
-            \think\facade\Db::getDbLog(true);
+        if (class_exists(ThinkDb::class, false) && class_exists(Mysql::class, false)) {
+            ThinkDb::getDbLog(true);
         }
-        if (!$initialized) {
+        if (!$initialized_db) {
+            $initialized_db = true;
             if (class_exists(QueryExecuted::class)) {
                 try {
                     Db::listen(function (QueryExecuted $query) {
@@ -55,6 +59,10 @@ class Middleware implements MiddlewareInterface
                     });
                 } catch (\Throwable $e) {echo $e;}
             }
+        }
+
+        if (!$initialized_redis) {
+            $initialized_redis = true;
             if (class_exists(CommandExecuted::class)) {
                 foreach (config('redis', []) as $key => $config) {
                     if (strpos($key, 'redis-queue') !== false) {
@@ -72,9 +80,7 @@ class Middleware implements MiddlewareInterface
                     } catch (\Throwable $e) {}
                 }
             }
-            $initialized = true;
         }
-
         $response = $next($request);
         $time_diff = substr((microtime(true) - $start_time)*1000, 0, 7);
         $logs .= " [{$time_diff}ms] [webman/log]" . PHP_EOL;
@@ -83,8 +89,8 @@ class Middleware implements MiddlewareInterface
         }
         $logs .= $this->logs;
 
-        if (class_exists(\think\facade\Db::class)) {
-            $sql_logs = \think\facade\Db::getDbLog(true);
+        if ($loaded_think_db = (class_exists(ThinkDb::class, false) && class_exists(Mysql::class, false))) {
+            $sql_logs = ThinkDb::getDbLog(true);
             if (!empty($sql_logs['sql'])) {
                 foreach ($sql_logs['sql'] as $sql) {
                     $logs .= "[SQL]\t" . trim($sql) . PHP_EOL;
@@ -96,12 +102,32 @@ class Middleware implements MiddlewareInterface
         if (method_exists($response, 'exception')) {
             $exception = $response->exception();
         }
-        if (config('plugin.webman.log.app.exception.enable') && $exception && !$this->shouldntReport($exception)) {
-            $logs .= $exception;
-            Log::error($logs);
-        } else {
-            Log::info($logs);
+
+        $method = 'info';
+
+        if ($exception && config('plugin.webman.log.app.exception.enable') && !$this->shouldntReport($exception)) {
+            $logs .= $exception . PHP_EOL;
+            $method = 'error';
         }
+
+        if (class_exists(Connection::class, false)) {
+            $pdo = Db::getPdo();
+            if ($pdo && $pdo->inTransaction()) {
+                Db::rollBack();
+                $method = 'error';
+                $logs .= "[ERROR]\tUncommitted transactions and rollback" . PHP_EOL;
+            }
+        }
+        if ($loaded_think_db) {
+            $pdo = ThinkDb::getPdo();
+            if ($pdo && $pdo->inTransaction()) {
+                Db::rollBack();
+                $method = 'error';
+                $logs .= "[ERROR]\tUncommitted transactions and rollback" . PHP_EOL;
+            }
+        }
+
+        call_user_func([Log::class, $method], $logs);
 
         return $response;
     }
