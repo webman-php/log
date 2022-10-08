7a176188bff3ba0089525180ccf8a3b79ce65228
sudden3 <49028616+sudden3@users.noreply.github.com>
2022-10-08 23:51:18 +0800

    增加默认不记录到日志的异常类 BusinessException

--- src/config/plugin/webman/log/app.php
+++ src/config/plugin/webman/log/app.php
@@ -4,7 +4,9 @@ return [
     'exception' => [
         // 是否记录异常到日志
         'enable' => true,
-        // 不会写入至日志的异常类
-        'dontReport' => []
+        // 不会记录到日志的异常类
+        'dontReport' => [
+            support\exception\BusinessException::class
+        ]
     ]
 ];
